f3cde29bea7a01f9ecb5da815a7d3cabc885aea4
David Schwelien <davidschwelien@web.de>
2019-03-22 07:34:05 +0100

    Sortings

--- execute.R
+++ execute.R
@@ -9,11 +9,24 @@ data <- data.frame(
   article.content = NA,
   article.title = NA,
   twitter.description = NA,
-  article.content = NA)
+  twitter.image = NA)
 
 `url_list` <- read.delim("~/Downloads/1552934988975_219259.csv", header=FALSE)
 url_list <- url_list$V2
 names(url_list) <- "article.url"
 
-url_list[1]
-get_srf_data(url[,1])
+for (url in url_list[1:1000]) {
+  tryCatch({
+    url.now <- toString(paste(url))
+    get_srf_data(url.now)
+    temp <- data.frame(
+      article.url = url,
+      article.content = article.content,
+      article.title = article.title,
+      twitter.description = twitter.description,
+      twitter.image = twitter.image)
+    data <- rbind(data,temp)
+  }, error=function(e){})
+}
+data <- data[-1,]
+#View(data)
